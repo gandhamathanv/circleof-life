d3f5741134eb8de5e99f234caaced2a4fd6aed6c
Gokul-Mylsami <gokulmylsami@gmail.com>
2022-10-14 13:37:35 +0530

    succes

--- scrip.js
+++ scrip.js
@@ -3,125 +3,125 @@ let passwordIndex = 0;
 const keys = Array.from(Array(10).keys());
 
 const model = [
-    { colour: "red", inner: 1, outer: 2 },
-    { colour: "green", inner: 3, outer: 4 },
-    { colour: "blue", inner: 5, outer: 6 },
-    { colour: "yellow", inner: 7, outer: 8 },
-    { colour: "violet", inner: 9, outer: 10 },
+  { colour: "red", inner: 1, outer: 2 },
+  { colour: "green", inner: 3, outer: 4 },
+  { colour: "blue", inner: 5, outer: 6 },
+  { colour: "yellow", inner: 7, outer: 8 },
+  { colour: "pink", inner: 9, outer: 10 },
 ];
 
 const outerCircle = document.getElementById("outer-circle");
 // LIST OF COLOURS
 const list = ["1", "2", "3", "4", "5"].map((el) =>
-    document.getElementsByClassName(el)
+  document.getElementsByClassName(el)
 );
 const num = [
-    "num-1",
-    "num-2",
-    "num-3",
-    "num-4",
-    "num-5",
-    "num-6",
-    "num-7",
-    "num-8",
-    "num-9",
-    "num-10",
+  "num-1",
+  "num-2",
+  "num-3",
+  "num-4",
+  "num-5",
+  "num-6",
+  "num-7",
+  "num-8",
+  "num-9",
+  "num-10",
 ].map((el) => document.getElementsByClassName(el));
 
 // SET COLOR
 
 const setColor = (colour, li) => {
-    Array.prototype.forEach.call(li, (child) => {
-        child.style.background = colour;
-        // console.log(child.style.background);
-    });
+  Array.prototype.forEach.call(li, (child) => {
+    child.style.background = colour;
+    // console.log(child.style.background);
+  });
 };
 const setNumber = (number, li) => {
-    Array.prototype.forEach.call(li, (child) => {
-        child.innerHTML = number;
-    });
+  Array.prototype.forEach.call(li, (child) => {
+    child.innerHTML = number;
+  });
 };
 
 function getRandomArbitrary(min, max) {
-    return Math.random() * (max - min) + min;
+  return Math.random() * (max - min) + min;
 }
 const getRandom = (key) => {
-    let keysRemove = key;
-    console.log(keysRemove);
-    let index = Math.floor(getRandomArbitrary(0, keysRemove.length + 1));
-    let value = keysRemove[index];
-    while (value == undefined) {
-        index = Math.floor(getRandomArbitrary(0, keysRemove.length + 1));
-        value = keysRemove[index];
-    }
-    keysRemove = keysRemove.filter((_, ind) => ind != index);
-    return { keysRemove, value };
+  let keysRemove = key;
+  console.log(keysRemove);
+  let index = Math.floor(getRandomArbitrary(0, keysRemove.length + 1));
+  let value = keysRemove[index];
+  while (value == undefined) {
+    index = Math.floor(getRandomArbitrary(0, keysRemove.length + 1));
+    value = keysRemove[index];
+  }
+  keysRemove = keysRemove.filter((_, ind) => ind != index);
+  return { keysRemove, value };
 };
 const setRandom = () => {
-    let key = keys;
+  let key = keys;
 
-    // SET 10 NUMBER
-    for (let index = 0; index < 10; index++) {
-        let data = getRandom(key);
-        let value = data.value;
-        key = data.keysRemove;
+  // SET 10 NUMBER
+  for (let index = 0; index < 10; index++) {
+    let data = getRandom(key);
+    let value = data.value;
+    key = data.keysRemove;
 
-        switch (index) {
-            case 0:
-                model[0].inner = value;
-                break;
-            case 1:
-                model[0].outer = value;
-                break;
-            case 2:
-                model[1].inner = value;
-                break;
-            case 3:
-                model[1].outer = value;
-                break;
-            case 4:
-                model[2].inner = value;
-                break;
-            case 5:
-                model[2].outer = value;
-                break;
-            case 6:
-                model[3].inner = value;
-                break;
-            case 7:
-                model[3].outer = value;
-                break;
-            case 8:
-                model[4].inner = value;
-                break;
-            case 9:
-                model[4].outer = value;
-                break;
+    switch (index) {
+      case 0:
+        model[0].inner = value;
+        break;
+      case 1:
+        model[0].outer = value;
+        break;
+      case 2:
+        model[1].inner = value;
+        break;
+      case 3:
+        model[1].outer = value;
+        break;
+      case 4:
+        model[2].inner = value;
+        break;
+      case 5:
+        model[2].outer = value;
+        break;
+      case 6:
+        model[3].inner = value;
+        break;
+      case 7:
+        model[3].outer = value;
+        break;
+      case 8:
+        model[4].inner = value;
+        break;
+      case 9:
+        model[4].outer = value;
+        break;
 
-            default:
-                break;
-        }
+      default:
+        break;
     }
+  }
 };
 setRandom();
 const UI = () => {
-    model.forEach((el, index) => {
-        // console.log({ el }, index, index + 1);
-        setColor(el.colour, list[index]);
-        setColor(el.colour, list[index]);
-        // console.log("setnumber", 2 * index + 1);
-        try {
-            setNumber(el.outer, num[2 * index]);
-            setNumber(el.inner, num[2 * index + 1]);
-        } catch (error) {
-            console.log(error);
-        }
-    });
+  model.forEach((el, index) => {
+    // console.log({ el }, index, index + 1);
+    setColor(el.colour, list[index]);
+    setColor(el.colour, list[index]);
+    // console.log("setnumber", 2 * index + 1);
+    try {
+      setNumber(el.outer, num[2 * index]);
+      setNumber(el.inner, num[2 * index + 1]);
+    } catch (error) {
+      console.log(error);
+    }
+  });
 };
 
 // SET UI
 const rotate = () => {
-    console.log(outerCircle);
+  console.log(outerCircle);
 };
 UI();
 
@@ -129,62 +129,66 @@ UI();
 let deg = 0;
 
 function left() {
-    let temp = model[0].colour;
-    model[0].colour = model[1].colour;
-    model[1].colour = model[2].colour;
-    model[2].colour = model[3].colour;
-    model[3].colour = model[4].colour;
-    model[4].colour = temp;
-    UI();
+  let temp = model[0].colour;
+  model[0].colour = model[1].colour;
+  model[1].colour = model[2].colour;
+  model[2].colour = model[3].colour;
+  model[3].colour = model[4].colour;
+  model[4].colour = temp;
+  UI();
 }
 
 function right() {
-    let temp = model[4].colour;
-    model[4].colour = model[3].colour;
-    model[3].colour = model[2].colour;
-    model[2].colour = model[1].colour;
-    model[1].colour = model[0].colour;
-    model[0].colour = temp;
-    UI();
+  let temp = model[4].colour;
+  model[4].colour = model[3].colour;
+  model[3].colour = model[2].colour;
+  model[2].colour = model[1].colour;
+  model[1].colour = model[0].colour;
+  model[0].colour = temp;
+  UI();
 }
 
 function getInner() {
-    const value = model.find((el) => el.colour == baseColor);
-    if (passwordIndex == 0) {
-        document.getElementById("opt-1").value = value.inner;
-    } else if (passwordIndex == 1) {
-        document.getElementById("opt-2").value = value.inner;
-    } else if (passwordIndex == 2) {
-        document.getElementById("opt-3").value = value.inner;
-    } else if (passwordIndex == 3) {
-        document.getElementById("opt-4").value = value.inner;
-    }
-    passwordIndex += 1;
+  const value = model.find((el) => el.colour == baseColor);
+  if (passwordIndex == 0) {
+    document.getElementById("opt-1").value = value.inner;
+  } else if (passwordIndex == 1) {
+    document.getElementById("opt-2").value = value.inner;
+  } else if (passwordIndex == 2) {
+    document.getElementById("opt-3").value = value.inner;
+  } else if (passwordIndex == 3) {
+    document.getElementById("opt-4").value = value.inner;
+  }
+  passwordIndex += 1;
 }
 
 function getOuter() {
-    const value = model.find((el) => el.colour == baseColor);
-    if (passwordIndex == 0) {
-        document.getElementById("opt-1").value = value.outer;
-    } else if (passwordIndex == 1) {
-        document.getElementById("opt-2").value = value.outer;
-    } else if (passwordIndex == 2) {
-        document.getElementById("opt-3").value = value.outer;
-    } else if (passwordIndex == 3) {
-        document.getElementById("opt-4").value = value.outer;
-    }
-    passwordIndex += 1;
+  const value = model.find((el) => el.colour == baseColor);
+  if (passwordIndex == 0) {
+    document.getElementById("opt-1").value = value.outer;
+  } else if (passwordIndex == 1) {
+    document.getElementById("opt-2").value = value.outer;
+  } else if (passwordIndex == 2) {
+    document.getElementById("opt-3").value = value.outer;
+  } else if (passwordIndex == 3) {
+    document.getElementById("opt-4").value = value.outer;
+  }
+  passwordIndex += 1;
 }
 
 function submit() {
-    const value1 = document.getElementById("opt-1").value;
-    const value2 = document.getElementById("opt-2").value;
-    const value3 = document.getElementById("opt-3").value;
-    const value4 = document.getElementById("opt-4").value;
-    const password =
-        String(value1) + String(value2) + String(value3) + String(value4);
-    console.log(Number(password));
-    console.log(Number(password) === numberNumber);
+  const value1 = document.getElementById("opt-1").value;
+  const value2 = document.getElementById("opt-2").value;
+  const value3 = document.getElementById("opt-3").value;
+  const value4 = document.getElementById("opt-4").value;
+  const password =
+    String(value1) + String(value2) + String(value3) + String(value4);
+  console.log(Number(password));
+  if (Number(password) === numberNumber) {
+    location.href = "./success.html";
+  } else {
+    location.href = "./failure.html";
+  }
 }
 // document.getElementById("spinner").style.display = "block";
 
@@ -197,71 +201,71 @@ let clipData;
 inputs[0].focus();
 
 inputs.forEach((tag, index) => {
-    tag.addEventListener("keyup", (event) => {
-        if (event.code === "Backspace" && hasNoValue(index)) {
-            if (index > 0) inputs[index - 1].focus();
-        }
+  tag.addEventListener("keyup", (event) => {
+    if (event.code === "Backspace" && hasNoValue(index)) {
+      if (index > 0) inputs[index - 1].focus();
+    }
 
-        //else if any input move focus to next or out
-        else if (tag.value !== "") {
-            index < inputs.length - 1 ? inputs[index + 1].focus() : tag.blur();
-        }
+    //else if any input move focus to next or out
+    else if (tag.value !== "") {
+      index < inputs.length - 1 ? inputs[index + 1].focus() : tag.blur();
+    }
 
-        //add val to array to track prev vals
-        values[index] = event.target.value;
-    });
+    //add val to array to track prev vals
+    values[index] = event.target.value;
+  });
 
-    tag.addEventListener("input", () => {
-        //replace digit if already exists
-        if (tag.value > 10) {
-            tag.value = tag.value % 10;
-        }
-    });
+  tag.addEventListener("input", () => {
+    //replace digit if already exists
+    if (tag.value > 10) {
+      tag.value = tag.value % 10;
+    }
+  });
 
-    tag.addEventListener("paste", (event) => {
-        event.preventDefault();
-        clipData = event.clipboardData.getData("text/plain").split("");
-        filldata(index);
-    });
+  tag.addEventListener("paste", (event) => {
+    event.preventDefault();
+    clipData = event.clipboardData.getData("text/plain").split("");
+    filldata(index);
+  });
 });
 
 function filldata(index) {
-    for (let i = index; i < inputs.length; i++) {
-        inputs[i].value = clipData.shift();
-    }
+  for (let i = index; i < inputs.length; i++) {
+    inputs[i].value = clipData.shift();
+  }
 }
 
 function hasNoValue(index) {
-    if (values[index] || values[index] === 0) return false;
+  if (values[index] || values[index] === 0) return false;
 
-    return true;
+  return true;
 }
 const correctPassword = window.prompt("enter a number");
-baseColor = Number(window.prompt("1.red\n2.green\n3.blue\n4.yellow\n5.violet"));
+baseColor = Number(window.prompt("1.red\n2.green\n3.blue\n4.yellow\n5.pink"));
 let numberNumber = null;
 const setPassword = () => {
-    numberNumber = Number(correctPassword);
-    switch (baseColor) {
-        case 1:
-            baseColor = "red";
+  numberNumber = Number(correctPassword);
+  switch (baseColor) {
+    case 1:
+      baseColor = "red";
 
-            break;
-        case 2:
-            baseColor = "green";
-            break;
-        case 3:
-            baseColor = "blue";
-            break;
-        case 4:
-            baseColor = "yellow";
-            break;
-        case 5:
-            baseColor = "violet";
-            break;
+      break;
+    case 2:
+      baseColor = "green";
+      break;
+    case 3:
+      baseColor = "blue";
+      break;
+    case 4:
+      baseColor = "yellow";
+      break;
+    case 5:
+      baseColor = "pink";
+      break;
 
-        default:
-            break;
-    }
+    default:
+      break;
+  }
 };
 
 setPassword();
